cb9934d58d5922152662f329741d7312b687ded9
e7 <jackzxty@126.com>
2017-10-15 08:42:35 +0800

    增加命令行参数处理

--- main.go
+++ main.go
@@ -1,15 +1,38 @@
 package main
 
 import (
-    "net/http"
+    "flag"
+    "fmt"
     "log"
+    "net/http"
+    "os"
+)
+
+var (
+    flag_help bool
+    flag_basedir string
+    flag_host string
+    flag_port string
 )
 
 
 func main() {
     var err error
 
-    err = http.ListenAndServe(":9002", nil)
+    // flags
+    flag.BoolVar(&flag_help, "h", false, "show this message")
+    flag.Usage = func () {
+        fmt.Fprintf(os.Stderr, "usage: xxxxx\n")
+    }
+    flag.StringVar(&flag_basedir, "b", ".", "base directory")
+    flag.Parse()
+
+    if flag_help {
+        flag.Usage()
+        return
+    }
+
+    err = http.ListenAndServe("127.0.0.1:9002", nil)
     if nil != err {
         log.Fatalf("[FATAL] %s", err)
     }
